bf1442b9860405edde468394c38c5ce751102c78
sonu <sonusaini0506@gmail.com>
2020-05-08 12:53:28 +0530

    add code to heroku

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -3,6 +3,7 @@ const UserSchema = mongoose.Schema({
     mobile: String,
     name: String,
     password: String,
+    type: {type:String,default:"user"},
     status:{type:Boolean,default:true},
     created_At:{ type: Date, required: true, default: Date.now }
 }, {
