f1dd0bf4b62f28a0750072842eca8a48918e248e
sonu <sonusaini0506@gmail.com>
2020-05-13 19:27:29 +0530

    time

--- app/controllers/createNumber.controller.js
+++ app/controllers/createNumber.controller.js
@@ -52,7 +52,7 @@ createNumberModule.save()
 }else{
     console.log("id",createNumberModule[0]._id);
 var id=createNumberModule[0]._id;
-    var updateNumber=await Gamenumber.updateOne({"createdBy":req.body.createdBy},req.body)
+    var updateNumber=await Gamenumber.updateOne({"_id":id},req.body)
     console.log(updateNumber);
     if(updateNumber){
     var resData={"status":"200",
